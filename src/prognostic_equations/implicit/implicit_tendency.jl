70369fd4217de6aa206d65055f711246e29b2d4e
Dennis Yatunin <dyatun@gmail.com>
2025-02-02 23:40:35 -0800

    Change density on cell faces to match dycore paper

--- src/prognostic_equations/implicit/implicit_tendency.jl
+++ src/prognostic_equations/implicit/implicit_tendency.jl
@@ -50,43 +50,51 @@ end
 
 function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:none})
     ᶜJ = Fields.local_geometry_field(ᶜρ).J
-    return @lazy @. -(ᶜadvdivᵥ(ᶠwinterp(ᶜJ, ᶜρ) * ᶠu³ * ᶠinterp(ᶜχ)))
+    ᶠJ = Fields.local_geometry_field(ᶠu³).J
+    return @lazy @. -(ᶜadvdivᵥ(ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠu³ * ᶠinterp(ᶜχ)))
 end
 function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:first_order})
     ᶜJ = Fields.local_geometry_field(ᶜρ).J
-    return @lazy @. -(ᶜadvdivᵥ(ᶠwinterp(ᶜJ, ᶜρ) * ᶠupwind1(ᶠu³, ᶜχ)))
+    ᶠJ = Fields.local_geometry_field(ᶠu³).J
+    return @lazy @. -(ᶜadvdivᵥ(ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠupwind1(ᶠu³, ᶜχ)))
 end
 @static if pkgversion(ClimaCore) ≥ v"0.14.22"
     function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:vanleer_limiter})
         ᶜJ = Fields.local_geometry_field(ᶜρ).J
+        ᶠJ = Fields.local_geometry_field(ᶠu³).J
         return @lazy @. -(ᶜadvdivᵥ(
-            ᶠwinterp(ᶜJ, ᶜρ) * ᶠlin_vanleer(ᶠu³, ᶜχ, dt),
+            ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠlin_vanleer(ᶠu³, ᶜχ, dt),
         ))
     end
 end
 function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:third_order})
     ᶜJ = Fields.local_geometry_field(ᶜρ).J
-    return @lazy @. -(ᶜadvdivᵥ(ᶠwinterp(ᶜJ, ᶜρ) * ᶠupwind3(ᶠu³, ᶜχ)))
+    ᶠJ = Fields.local_geometry_field(ᶠu³).J
+    return @lazy @. -(ᶜadvdivᵥ(ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠupwind3(ᶠu³, ᶜχ)))
 end
 function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:boris_book})
     ᶜJ = Fields.local_geometry_field(ᶜρ).J
+    ᶠJ = Fields.local_geometry_field(ᶠu³).J
     return @lazy @. -(ᶜadvdivᵥ(
-        ᶠwinterp(ᶜJ, ᶜρ) * (
+        ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * (
             ᶠupwind1(ᶠu³, ᶜχ) + ᶠfct_boris_book(
                 ᶠupwind3(ᶠu³, ᶜχ) - ᶠupwind1(ᶠu³, ᶜχ),
-                ᶜχ / dt - ᶜadvdivᵥ(ᶠwinterp(ᶜJ, ᶜρ) * ᶠupwind1(ᶠu³, ᶜχ)) / ᶜρ,
+                ᶜχ / dt -
+                ᶜadvdivᵥ(ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠupwind1(ᶠu³, ᶜχ)) / ᶜρ,
             )
         ),
     ))
 end
 function vertical_transport(ᶜρ, ᶠu³, ᶜχ, dt, ::Val{:zalesak})
     ᶜJ = Fields.local_geometry_field(ᶜρ).J
+    ᶠJ = Fields.local_geometry_field(ᶠu³).J
     return @lazy @. -(ᶜadvdivᵥ(
-        ᶠwinterp(ᶜJ, ᶜρ) * (
+        ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * (
             ᶠupwind1(ᶠu³, ᶜχ) + ᶠfct_zalesak(
                 ᶠupwind3(ᶠu³, ᶜχ) - ᶠupwind1(ᶠu³, ᶜχ),
                 ᶜχ / dt,
-                ᶜχ / dt - ᶜadvdivᵥ(ᶠwinterp(ᶜJ, ᶜρ) * ᶠupwind1(ᶠu³, ᶜχ)) / ᶜρ,
+                ᶜχ / dt -
+                ᶜadvdivᵥ(ᶠinterp(ᶜJ * ᶜρ) / ᶠJ * ᶠupwind1(ᶠu³, ᶜχ)) / ᶜρ,
             )
         ),
     ))
@@ -104,33 +112,35 @@ function implicit_vertical_advection_tendency!(Yₜ, Y, p, t)
     (; dt) = p
     n = n_mass_flux_subdomains(turbconv_model)
     ᶜJ = Fields.local_geometry_field(Y.c).J
+    ᶠJ = Fields.local_geometry_field(Y.f).J
     (; ᶠgradᵥ_ᶜΦ) = p.core
     (; ᶜh_tot, ᶜspecific, ᶠu³, ᶜp) = p.precomputed
     (; ᶜwₜqₜ, ᶜwₕhₜ) = p.precomputed
 
-    @. Yₜ.c.ρ -= ᶜdivᵥ(ᶠwinterp(ᶜJ, Y.c.ρ) * ᶠu³)
-    @. Yₜ.c.ρ -= ᶜprecipdivᵥ(ᶠwinterp(ᶜJ, Y.c.ρ) * ᶠright_bias(-(ᶜwₜqₜ)))
+    @. Yₜ.c.ρ -= ᶜdivᵥ(ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ * ᶠu³)
+    @. Yₜ.c.ρ -= ᶜprecipdivᵥ(ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ * ᶠright_bias(-(ᶜwₜqₜ)))
 
     # Central advection of active tracers (e_tot and q_tot)
     vtt = vertical_transport(Y.c.ρ, ᶠu³, ᶜh_tot, dt, Val(:none))
     @. Yₜ.c.ρe_tot += vtt
-    @. Yₜ.c.ρe_tot -= ᶜprecipdivᵥ(ᶠwinterp(ᶜJ, Y.c.ρ) * ᶠright_bias(-(ᶜwₕhₜ)))
+    @. Yₜ.c.ρe_tot -=
+        ᶜprecipdivᵥ(ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ * ᶠright_bias(-(ᶜwₕhₜ)))
 
     if !(moisture_model isa DryModel)
         vtt = vertical_transport(Y.c.ρ, ᶠu³, ᶜspecific.q_tot, dt, Val(:none))
         @. Yₜ.c.ρq_tot += vtt
         @. Yₜ.c.ρq_tot -=
-            ᶜprecipdivᵥ(ᶠwinterp(ᶜJ, Y.c.ρ) * ᶠright_bias(-(ᶜwₜqₜ)))
+            ᶜprecipdivᵥ(ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ * ᶠright_bias(-(ᶜwₜqₜ)))
     end
 
     if moisture_model isa NonEquilMoistModel
         (; ᶜwₗ, ᶜwᵢ) = p.precomputed
         @. Yₜ.c.ρq_liq -= ᶜprecipdivᵥ(
-            ᶠwinterp(ᶜJ, Y.c.ρ) *
+            ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ *
             ᶠright_bias(Geometry.WVector(-(ᶜwₗ)) * ᶜspecific.q_liq),
         )
         @. Yₜ.c.ρq_ice -= ᶜprecipdivᵥ(
-            ᶠwinterp(ᶜJ, Y.c.ρ) *
+            ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ *
             ᶠright_bias(Geometry.WVector(-(ᶜwᵢ)) * ᶜspecific.q_ice),
         )
     end
@@ -142,11 +152,11 @@ function implicit_vertical_advection_tendency!(Yₜ, Y, p, t)
         # using downward biasing and free outflow bottom boundary condition
         (; ᶜwᵣ, ᶜwₛ) = p.precomputed
         @. Yₜ.c.ρq_rai -= ᶜprecipdivᵥ(
-            ᶠwinterp(ᶜJ, Y.c.ρ) *
+            ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ *
             ᶠright_bias(Geometry.WVector(-(ᶜwᵣ)) * ᶜspecific.q_rai),
         )
         @. Yₜ.c.ρq_sno -= ᶜprecipdivᵥ(
-            ᶠwinterp(ᶜJ, Y.c.ρ) *
+            ᶠinterp(ᶜJ * Y.c.ρ) / ᶠJ *
             ᶠright_bias(Geometry.WVector(-(ᶜwₛ)) * ᶜspecific.q_sno),
         )
     end
